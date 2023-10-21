dcf4bfc5eb5d1674d20870e82bed01124a20aa53
Justin Yu <justin741yu@gmail.com>
2023-10-21 03:44:35 -0700

    Created Code base

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,43 @@
-const express = require("express");
-const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3000;
 const rootRoutes = require("./routes/root");
 const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
 const requestTime = require("./middleware/request-time");
+const morgan = require("morgan");
 
+const express = require("express");
+const app = express();
 
-
+app.use(morgan("dev"));
+app.use(requestTime);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "static")));
-app.use(requestTime);
-app.use("/", rootRoutes);
-app.use((request, response, next) => {
-    next(createError(404));
-});
 
 if (process.env.NODE_ENV == "development") {
-    const livereload = require("livereload");
-    const connectLiveReload = require("connect-livereload");
-    const liveReloadServer = livereload.createServer();
-    liveReloadServer.watch(path.join(__dirname, "backend", "static"));
-    liveReloadServer.server.once("connection", () => {
-        setTimeout(() => {
-            liveReloadServer.refresh("/");
-        }, 100);
-    });
-    app.use(connectLiveReload());
+  const livereload = require("livereload");
+  const connectLiveReload = require("connect-livereload");
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "backend", "static"));
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+  app.use(connectLiveReload());
 }
 
+app.use("/", rootRoutes);
 
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
+});
+
+app.use((request, response, next) => {
+  next(createError(404));
 });
